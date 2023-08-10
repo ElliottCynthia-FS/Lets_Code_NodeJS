4fe42080df62785418c95cdc2be796df307563b6
ElliottCynthia-FS <caelliott@student.fullsail.edu>
2023-08-10 18:55:27 -0400

    new test for the string module - all passed

---
+++ string/string.js
@@ -0,0 +1,13 @@
+// instead of running this:
+// const uppercase = (str) => {
+    // return str.toUpperCase();
+// }
+// Run one-line code like this:
+const uppercase = (str) => str.toUpperCase();
+
+const lowercase = (str) => str.toLowerCase();
+
+module.exports = {
+    uppercase,
+    lowercase
+}
